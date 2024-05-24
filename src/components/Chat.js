4ac76d62201aa241e4cbc828bc5e5e984cbf315d
divyank-jain-singhvi <you@example.com>
2024-05-24 18:15:05 +0530

    added connected to frontend with get and post

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import axios from 'axios';
 import './Chat.css'
 
 export default function Chat() {
-  const handleSubmit = () => {
-    console.log("hello")
-  }
+
+  const[question,setQuestion]=useState("");
+    const[outputs,setoutputs]=useState("Terminal");
+    const SendQuestion = async () => {
+    console.log(question)
+    let QuestionBlock = {
+        'question': 'hello',
+        'answer': 'Terminal',
+    };
+        await axios({
+            method: 'post',
+            url: 'http://localhost:8000/',
+            data: QuestionBlock,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.error("Error:", error);
+        });
+        const response=await axios.get('http://localhost:8000/')
+        console.log(response.data)
+        setoutputs(response.data)
+    };
   return (
     <>
         <div className='chat-main'>
             <div className='one-chat-div'>
-                <div></div>
-                <div></div>
+                <div>{outputs[Object.keys(outputs).length-1].question}</div>
+                <div>{outputs[Object.keys(outputs).length-1].answer}</div>
             </div>
             <div className='question-input-block'>
-                <input className='question-input shadow' placeholder='Send a message...' type="text"></input>
-                <button onClick={handleSubmit} className='submit-btn'>→</button>
+                <input className='question-input shadow' placeholder='Send a message...' type="text" onChange={(e)=>setQuestion(e.target.value)} value={question} required></input>
+                <button onClick={SendQuestion} className='submit-btn'>→</button>
             </div>
 
             </div>
